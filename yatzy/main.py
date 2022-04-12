7c92ce4c7f2ef49114daceb2c17143d8b7304f83
Marcus Veibäck <sirmar@gmail.com>
2022-04-12 20:52:12 +0200

    Refactor main

--- yatzy/main.py
+++ yatzy/main.py
@@ -1,19 +1,18 @@
-from yatzy.games import Yatzy, MaxiYatzy
+from yatzy.games import Game, Yatzy, MaxiYatzy
 from yatzy.engine import Engine
 from yatzy.cli import CLI
 from yatzy.hand import Hand
 from yatzy.board import Board
 
 
-def yatzy():
-    engine = Engine(CLI(), Hand(5), Board(Yatzy()))
+def play(game: Game) -> None:
+    engine = Engine(CLI(), Hand(game.nb_of_dice), Board(game))
     engine.play()
+
+
+def yatzy():
+    play(Yatzy())
 
 
 def maxi_yatzy():
-    engine = Engine(CLI(), Hand(5), Board(MaxiYatzy()))
-    engine.play()
-
-
-if __name__ == "__main__":
-    yatzy()
+    play(MaxiYatzy())
